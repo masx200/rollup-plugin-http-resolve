95cb8f44c5742c929c8a2576c9ebd09725a75b56
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-02 14:23:58 +0800

    Create index.d.ts

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+import { Plugin } from "rollup";
+export declare type ResolveIdFallback = (specifier: string, importer?: string) => string | void;
+declare type HttpResolveOptions = {
+    cache?: any;
+    fetcher?: (url: string) => Promise<string>;
+    onRequest?: (url: string) => void;
+    onUseCache?: (url: string) => void;
+    resolveIdFallback?: ResolveIdFallback;
+};
+export declare const httpResolve: ({ cache, onRequest, onUseCache, fetcher, resolveIdFallback, }?: HttpResolveOptions) => Plugin;
+export {};
