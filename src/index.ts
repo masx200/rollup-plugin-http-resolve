5c63790ce474e0f69be3be41f549a942aae70ae1
masx200 <masx200@qq.com>
2022-12-30 10:37:21 +0800

    "User-Agent"

--- src/index.ts
+++ src/index.ts
@@ -85,7 +85,12 @@ export const httpResolve = function httpResolve_({
                     await cache.set(id, code);
                     return code;
                 } else {
-                    const res = await fetch(id,{headers:{"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"}});
+                    const res = await fetch(id, {
+                        headers: {
+                            "User-Agent":
+                                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54",
+                        },
+                    });
                     if (!res.ok) {
                         throw res.statusText;
                     }
